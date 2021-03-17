a60d72d90a493ea25db4316d3159071bfcacc22a
AhmedEhab1995 <ahmedsupera@gmail.com>
2021-03-17 02:42:23 +0200

    Connected the phone verification layout to the otp layout and too the main activity that will include the chats. Added the authentication code and tested it.

--- app/src/main/java/com/example/chattingwhatsapp/activity_otpmessageverification.java
+++ app/src/main/java/com/example/chattingwhatsapp/activity_otpmessageverification.java
@@ -1,14 +1,91 @@
 package com.example.chattingwhatsapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.chattingwhatsapp.databinding.ActivityOtpmessageverificationBinding;
+import com.example.chattingwhatsapp.databinding.ActivityPhoneNumberEntryBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.mukesh.OnOtpCompletionListener;
+
+import java.util.concurrent.TimeUnit;
 
 public class activity_otpmessageverification extends AppCompatActivity {
+
+    ActivityOtpmessageverificationBinding OtpActivityOtpmessageverificationBinding;
+    TextView phoneNumberMessage;
+    FirebaseAuth auth;
+    String verificationId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_otpmessageverification);
+        OtpActivityOtpmessageverificationBinding = ActivityOtpmessageverificationBinding.inflate(getLayoutInflater());
+        setContentView(OtpActivityOtpmessageverificationBinding.getRoot());
+
+        auth = FirebaseAuth.getInstance();
+        String phoneNumber = getIntent().getStringExtra("phone number");
+        phoneNumberMessage = findViewById(R.id.phonenumbermessage);
+        phoneNumberMessage.setText("Verify " + phoneNumber + " is your phone number");
+
+        PhoneAuthOptions phoneAuthOptions = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(activity_otpmessageverification.this)
+                .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        verificationId = s;
+                    }
+                }).build();
+
+        PhoneAuthProvider.verifyPhoneNumber(phoneAuthOptions);
+
+        OtpActivityOtpmessageverificationBinding.otpView.setOtpCompletionListener(new OnOtpCompletionListener() {
+            @Override
+            public void onOtpCompleted(String otp) {
+
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, otp);
+                auth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(activity_otpmessageverification.this, "Successfully logged in ", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(activity_otpmessageverification.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                        else{
+                            Toast.makeText(activity_otpmessageverification.this, "Wrong Otp", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/chattingwhatsapp/activity_phone_number_entry.java
+++ app/src/main/java/com/example/chattingwhatsapp/activity_phone_number_entry.java
@@ -1,14 +1,31 @@
 package com.example.chattingwhatsapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class activity_phone_number_entry extends AppCompatActivity {
+
+    Button send;
+    EditText phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_number_entry);
+        send = findViewById(R.id.sendButton);
+        phoneNumber = findViewById(R.id.phoneEntry);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_phone_number_entry.this, activity_otpmessageverification.class);
+                intent.putExtra("phone number", phoneNumber.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
