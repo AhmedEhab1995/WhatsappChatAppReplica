eb276dadba12c3269340b634c6f5209b74eca87f
AhmedEhab1995 <ahmedsupera@gmail.com>
2021-03-19 02:42:48 +0200

    Created the all chats layout, also connected the DB. Fixed the images to be circular.

--- app/src/main/java/com/example/chattingwhatsapp/MainActivity.java
+++ app/src/main/java/com/example/chattingwhatsapp/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.chattingwhatsapp;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.chattingwhatsapp.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+    FirebaseDatabase database;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<>();
+
+        usersAdapter = new UsersAdapter(this, users);
+        //activityMainBinding.listOfFirends.setLayoutManager(new LinearLayoutManager(this));
+        activityMainBinding.listOfFirends.setAdapter(usersAdapter);
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                    User user = snapshot1.getValue(User.class);
+                    users.add(user);
+                }
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.search) {
+            // do something
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
